80ca72b92c5b73acd1cb68acab1a804f01988ebe
hamidi bin ismail <hamidi4820@gmail.com>
2025-06-30 23:34:22 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# MyHSCode Checker v2
+This is the updated deployment-ready code for Vercel.
---
+++ index.js
@@ -0,0 +1,7 @@
+// Entry point React
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './src/App';
+
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
