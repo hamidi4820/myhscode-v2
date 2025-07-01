b9cb7b652bf2f064f5587ff34ec09054e045278b
hamidi bin ismail <hamidi4820@gmail.com>
2025-07-01 11:32:25 +0800

    delete old files

--- index.js
+++
@@ -1,7 +0,0 @@
-// Entry point React
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './src/App';
-
-const root = createRoot(document.getElementById('root'));
-root.render(<App />);
